5d8e573c1a6c8df728a8c87e9f251987a7cf8ec1
Pragadeeshwaran-k <pragadeeshwaran7sv@gmail.com>
2023-11-24 01:20:53 +0530

    next ticket booking concept poc initial commit

---
+++ nextjs-app-cms/app/TicketPage/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TicketPage = ({ params }: any) => {
+  return (
+    <>
+      <div>TicketPage {params.id}</div>
+    </>
+  );
+};
+
+export default TicketPage;
